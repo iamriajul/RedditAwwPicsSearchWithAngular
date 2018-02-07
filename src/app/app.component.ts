ccd00194040e8627beba6df0f304dd84f80211ad
Riajul Islam <kmriajulislami@gmail.com>
2018-02-07 15:24:04 +0600

    Basic needed html markup and angular expression for viewing images.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Aww Pics Search Engine';
+  searchString  = 'Cat';
+  results$: Observable<any>;
+
+  ngOnInit() {
+
+  }
+
+  onSearchStringChange($event) {
+    console.log(this.searchString);
+  }
 }
